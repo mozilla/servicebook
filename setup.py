d5360af08487d2f7de2789531c9903c4ac9dd8a9
Tarek Ziade <tarek@ziade.org>
2016-11-16 08:55:42 +0100

    distutils/tox/travis boiler plate

---
+++ setup.py
@@ -0,0 +1,16 @@
+import sys
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    deps = [dep for dep in f.read().split('\n') if dep.strip() != '']
+    install_requires = deps
+
+
+setup(name='servicebook',
+      version="0.1",
+      packages=find_packages(),
+      description="Mozilla QA Service Book",
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires)
